a552e61be24a8d6bb78f5ee5ecd1d51653473ec6
stevegaines <stevegaines@10.200.100.246>
2018-11-28 15:11:11 +0000

    Hello FAB

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import csv
+from flask import request
 from flask import Flask
 application = Flask(__name__)
 
@@ -7,3 +9,15 @@ def hello():
 
 if __name__ == "__main__":
     application.run()
+
+@application.route('/casestudy')
+def casestudy():
+    region = request.args.get('region')
+    product = request.args.get('product')
+    with open('casestudy.csv', 'r') as csvfile:
+        customers = csv.reader(csvfile, delimiter=',', quotechar='|')
+        customermatches=""
+        for row in customers:       
+            if row[1].find(region) != -1 and row[3].find(product) != -1:
+                customermatches = customermatches+ "<p><a href='"+row[4]+"'>"+row[0]+"</a>"
+    return "<h1>Matching customers</h1>\n<h3>"+customermatches+"</h3>"
